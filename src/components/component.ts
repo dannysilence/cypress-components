fb3f5040afe208a2a68da1b42c4da98c9f96b821
ds <48093199+dannysilence@users.noreply.github.com>
2022-06-18 21:09:07 +0300

    Update component.ts

--- src/components/component.ts
+++ src/components/component.ts
@@ -35,17 +35,3 @@
 
                 private _container: T;
             }
-
-            export class Factory {
-                // static fromTElement<TElement extends Element, TComponent extends Component<TElement>>(element: TElement, ctor: (new (base: TElement) => TComponent)): TComponent {
-                //     return new ctor(element);
-                // }
-
-                static fromElement<TComponent extends Component<Element>>(element: Element, ctor: (new (base: Element) => TComponent)): TComponent {
-                    return new ctor(element);
-                }
-
-                static fromHTMLElement<TComponent extends Component<HTMLElement>>(element: HTMLElement, ctor: (new (base: HTMLElement) => TComponent)): TComponent {
-                    return new ctor(element);
-                }
-            }
