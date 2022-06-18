b30c850125f0c73408298a6f9bd66e7aca8909d2
ds <48093199+dannysilence@users.noreply.github.com>
2022-06-18 21:10:46 +0300

    Update factory.ts

--- src/components/factory.ts
+++ src/components/factory.ts
@@ -1,13 +1,11 @@
-            export class Factory {
-                // static fromTElement<TElement extends Element, TComponent extends Component<TElement>>(element: TElement, ctor: (new (base: TElement) => TComponent)): TComponent {
-                //     return new ctor(element);
-                // }
+import { Component } from './component'
 
-                static fromElement<TComponent extends Component<Element>>(element: Element, ctor: (new (base: Element) => TComponent)): TComponent {
-                    return new ctor(element);
-                }
+export class Factory {
+    static fromElement<TComponent extends Component<Element>>(element: Element, ctor: (new (base: Element) => TComponent)): TComponent {
+        return new ctor(element);
+    }
 
-                static fromHTMLElement<TComponent extends Component<HTMLElement>>(element: HTMLElement, ctor: (new (base: HTMLElement) => TComponent)): TComponent {
-                    return new ctor(element);
-                }
-            }
+    static fromHTMLElement<TComponent extends Component<HTMLElement>>(element: HTMLElement, ctor: (new (base: HTMLElement) => TComponent)): TComponent {
+        return new ctor(element);
+    }
+}
