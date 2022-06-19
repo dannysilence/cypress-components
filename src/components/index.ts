8c73a35c72d9a800027a4bae05402aa57a5044a0
ds <48093199+dannysilence@users.noreply.github.com>
2022-06-19 11:48:29 +0300

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,35 @@
+import { Cypress } from 'cypress';
+import { Dropdown } from './dropdown';
+import { Table } from './table';
+
 export * from './component';
 export * from './factory';
 export * from './dropdown';
 export * from './table';
+
+Cypress.Commands.add<'dropdown', 'optional'>(
+    'dropdown',
+    { prevSubject: 'optional' },
+    (subject?:JQuery<HTMLElement>, locator = '', options?) => {
+        if(subject) {
+            const x = new Dropdown(subject.get()[0]);
+            return cy.wrap(x);
+        } 
+
+        return cy.get(locator).dropdown();
+    }
+  )
+
+Cypress.Commands.add<'dropdown', 'optional'>(
+    'table',
+    { prevSubject: 'optional' },
+    (subject?:JQuery<HTMLElement>, locator = '', options?) => {
+        if(subject) {
+            const x = new Table(subject.get()[0]);
+            return cy.wrap(x);
+        } 
+
+        return cy.get(locator).table();
+    }
+  )
+  
