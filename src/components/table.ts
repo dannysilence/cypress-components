6a64e55d4a56f8f29d61d4a9b04895b7bebaa6e8
ds <dannysilence@homelabs.company>
2022-06-23 18:50:50 +0300

    added getColumnData(), getColumnIndex() , getColumnNames() methods

--- src/components/table.ts
+++ src/components/table.ts
@@ -4,6 +4,35 @@ export class Table extends Component<HTMLElement> {
     constructor(element: HTMLElement) { 
         super(element);
     }
+
+    public getColumnNames(): string[] {
+        return Array.from(this.element.querySelectorAll('thead th')).map(e => e.textContent);
+    }
+
+    public getColumnIndex(id: number|string): number {
+        const names = this.getColumnNames();
+
+        if(typeof(id) === typeof(Number)) {
+            return (id as number);
+        }
+
+        for (let index = 0; index < names.length; index++) {
+            const element = names[index];
+            
+            if(element === id) {
+                return index;
+            }
+        }
+
+        return -1;
+    }
+
+    public getColumnData(id: number|string): string[] {
+        const col = this.getColumnIndex(id);
+        const items = Array.from(this.element.querySelectorAll(`tbody > tr > td:nth-child(${col+1})`)).map(e => e.textContent);
+
+        return  items;
+    }
 }
 
 export interface TableOptions extends ComponentOptions {
